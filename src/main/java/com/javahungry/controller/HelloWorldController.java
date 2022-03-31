308377e471b110f4a8463f82787d6f77f7bc5b65
AkashRepoWorkspace <56564618+AkashRepoWorkspace@users.noreply.github.com>
2022-03-31 11:27:50 +0530

    added second print line

--- src/main/java/com/javahungry/controller/HelloWorldController.java
+++ src/main/java/com/javahungry/controller/HelloWorldController.java
@@ -9,7 +9,7 @@ public class HelloWorldController {
 	@GetMapping(path="/")
 	public String sayHello() {
 		System.out.println("Added New Line ");
-		
+		System.out.println("Added Second Line ");
 		return "Hello!! from Java Hungry";
 	}
 
